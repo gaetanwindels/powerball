61c88db52e95140713a35bfef62f2b6a8117fcc6
gaetanwindels <gaetan.windels@gmail.com>
2017-03-08 14:15:08 +0100

    first commit

---
+++ Assets/scripts/FastMoveScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastMoveScript : MonoBehaviour {
+
+	public float cooldown = 3f;
+	public float speedFactor = 2;
+	public float duration = 0.2f;
+
+	private float showTime = 0;
+	private bool pressed = false;
+	private bool isCoolDown = false;
+	Color initialColor;
+
+	// Use this for initialization
+	void Start () {
+		initialColor = GetComponent<SpriteRenderer> ().color;
+	}
+	
+	// Update is called once per frame
+	void Update () {			
+		if (Input.GetKey(KeyCode.Space) && GetComponent<MoveScript> ().speedFactor == 1 && !isCoolDown) {
+			GetComponent<MoveScript> ().speedFactor = speedFactor;
+			GetComponent<SpriteRenderer> ().color = Color.black;
+			Invoke ("ResetSpeed", duration);
+		}
+	}
+
+	void ResetSpeed() {
+		GetComponent<MoveScript> ().speedFactor = 1;
+		isCoolDown = true;
+		Invoke ("ResetCoolDown", cooldown);
+		GetComponent<SpriteRenderer> ().color = initialColor;
+	}
+
+	void ResetCoolDown() {
+		isCoolDown = false;
+	}
+}
